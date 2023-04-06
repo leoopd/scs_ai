f7c3f60f5d313e99e05ea3740bf15671754dde01
leo <leo.opdenplatz@gmail.com>
2023-04-06 23:50:52 +0200

    changed sitemap_parser()
    now returns a list of links instead of writing them to a csv file in output

--- app/util.py
+++ app/util.py
@@ -8,8 +8,10 @@ def sitemap_parser(sitemap_path):
         soup = BeautifulSoup(f, 'xml')
         link_str = soup.text[soup.text.find('https'):soup.text.find(']')]
         link_list = link_str.split('\n')
-        with open('output/links.csv', 'w') as l:
-            writer = csv.writer(l, delimiter='#')
-            writer.writerow(link_list)
+        return link_list
         
 sitemap_parser(sitemap_path)
+
+def placeholder():
+    with open('output/links.csv') as f:
+        reader = csv.reader(f)
