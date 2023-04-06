717d28cfcc475c5b5c6e5b25d9971fb61d0dc4a7
leo <leo.opdenplatz@gmail.com>
2023-04-06 23:57:01 +0200

    added link_writer()
    takes in the link_list from sitemap_parser() and writes it to a csv in output. each link is written on a single line.

--- app/util.py
+++ app/util.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import csv
+import time
 
+timestr = time.strftime('%Y%m%d-%H%M%S')
 sitemap_path = 'sitemap.xml'
 
 def sitemap_parser(sitemap_path):
@@ -12,6 +14,10 @@ def sitemap_parser(sitemap_path):
         
 sitemap_parser(sitemap_path)
 
-def placeholder():
-    with open('output/links.csv') as f:
-        reader = csv.reader(f)
+def link_writer(link_list):
+    with open(f'output/links_{timestr}.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        for link in link_list:
+            writer.writerow([link])
+
+link_writer(sitemap_parser(sitemap_path))
