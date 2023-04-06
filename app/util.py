0c04a643639ec5837d8abbd0ec93df75d216cc7f
leo <leo.opdenplatz@gmail.com>
2023-04-07 00:33:31 +0200

    added html_grabber()
    downloads the provided websites and saves them as html in output with the link as name (/ replaced with _)

--- app/util.py
+++ app/util.py
@@ -1,9 +1,11 @@
 from bs4 import BeautifulSoup
 import csv
 import time
+import requests
 
 timestr = time.strftime('%Y%m%d-%H%M%S')
 sitemap_path = 'sitemap.xml'
+csv_file = 'links_20230406-235607.csv'
 
 def sitemap_parser(sitemap_path):
     with open(sitemap_path) as f:
@@ -11,13 +13,23 @@ def sitemap_parser(sitemap_path):
         link_str = soup.text[soup.text.find('https'):soup.text.find(']')]
         link_list = link_str.split('\n')
         return link_list
-        
-sitemap_parser(sitemap_path)
 
 def link_writer(link_list):
-    with open(f'output/links_{timestr}.csv', 'w', newline='') as f:
+    with open(f'links_{timestr}.csv', 'w', newline='') as f:
         writer = csv.writer(f)
         for link in link_list:
             writer.writerow([link])
 
-link_writer(sitemap_parser(sitemap_path))
+# link_writer(sitemap_parser(sitemap_path))
+
+def html_grabber(csv_file):
+    with open(csv_file) as f:
+        reader = csv.reader(f)
+        for row in reader:
+            link = row[0]
+            name = link[26:].replace('/', '_')
+            response = requests.get(link)
+            with open(f'output/{name}.html', 'w') as h:
+                h.write(response.text)
+
+html_grabber(csv_file)
