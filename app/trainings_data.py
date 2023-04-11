d3b5669e47518a3836c848708fc7b1e284dd8181
leo <leo.opdenplatz@gmail.com>
2023-04-11 19:54:52 +0200

    added docstrings to trainings_data.py

--- app/trainings_data.py
+++ app/trainings_data.py
@@ -7,6 +7,11 @@ file_path = 'csv/artiles_20230409-221317.csv'
 output_path = f'json/trainings_data_{timestr}.jsonl'
 
 def prompt_list_maker(file_path):
+    '''
+    Reads in the csv file containing articles returned by articles_to_csv() in articles.py, extracts category, link and article and
+    appends them to a list that gets returned. The list will have one element of the original list in one line, formatted
+    according to openai trainings data requirements.
+    '''
     prompt_list = []
     with open(file_path, 'r') as f:
         reader = csv.reader(f, delimiter='#')
@@ -19,6 +24,9 @@ def prompt_list_maker(file_path):
         return prompt_list
 
 def prompt_list_writer(prompt_list, output_path):
+    '''
+    Writes the list of prompts returned by prompt_list_maker() and writes them to a jsonl file.
+    '''
     with open(output_path, 'w') as f:
         for element in prompt_list:
             f.write(json.dumps(element, ensure_ascii=False) + "\n")
