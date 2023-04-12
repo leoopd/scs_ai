707234494d37acc07f23db2a6f9bd62ce5e120fb
leo <leo.opdenplatz@gmail.com>
2023-04-12 15:10:44 +0200

    changed functionality
    removed link_writer() since there is no need to write a csv file containing the links
    changed html_grabber() to work on a list of links returned by sitemap_parser() instead of a csv file

--- app/links.py
+++ app/links.py
@@ -16,28 +16,23 @@ def sitemap_parser(sitemap_path):
         link_list = link_str.split('\n')
         return link_list
 
-def link_writer(link_list):
-    '''
-    Writes the link list returned by sitemap_parser() to a csv.
-    '''
-    with open(f'links_{timestr}.csv', 'w', newline='') as f:
-        writer = csv.writer(f)
-        for link in link_list:
-            writer.writerow([link])
+# def link_writer(link_list):
+#     '''
+#     Writes the link list returned by sitemap_parser() to a csv.
+#     '''
+#     with open(f'links_{timestr}.csv', 'w', newline='') as f:
+#         writer = csv.writer(f)
+#         for link in link_list:
+#             writer.writerow([link])
 
-# link_writer(sitemap_parser(sitemap_path))
-
-def html_grabber(csv_file):
+def html_grabber(link_list, dest_folder='html/'):
     '''
-    Reads the csv returned by link_writer, downloads each webpage and saves it as an html file in /html.
+    Reads a list of links, downloads each webpage and saves it as an html file in the specified folder (default=/html).
     '''
-    with open(csv_file) as f:
-        reader = csv.reader(f)
-        for row in reader:
-            link = row[0]
-            name = link[26:].replace('/', '_')
-            response = requests.get(link)
-            with open(f'html/{name}.html', 'w') as h:
-                h.write(response.text)
+    for link in link_list:
+        name = link[26:].replace('/', '_')
+        response = requests.get(link)
+        with open(f'html/{name}.html', 'w') as h:
+            h.write(response.text)
 
-# html_grabber(csv_file)
+html_grabber(sitemap_parser(sitemap_path))
