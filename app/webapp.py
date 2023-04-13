c72583ce28ef25eb9d244d44afdd7ad68610499f
leo <leo.opdenplatz@gmail.com>
2023-04-13 21:34:17 +0200

    added webapp.py
    will display the form that allows the user to input a prompt and receive the response from the ai model

---
+++ app/webapp.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template
+import openai
+
+app = Flask(__name__)
+
+@app.route('/scsai', methods=['POST'])
+def scs_ai():
+    user_input = request.form['user_input']
+
+    response = openai.Completion.create(
+        engine='scs-faq-domains-2023-04-13-09-28-43',
+        prompt=user_input,
+        temperature=0,
+        max_tokens=1000,
+        n=1,
+        stop=[" ###"]
+    )
+
+    ai_output = response.choices[0].text.strip()
+
+    return render_template('ai_template.html', output=ai_output)
+
