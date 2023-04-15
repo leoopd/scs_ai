09d3f18247c6290a26af1857e436a7ec48655251
Leo <109588159+leoopd@users.noreply.github.com>
2023-04-15 20:14:46 +0200

    removed app/
    files no longer needed, since webapp.py contains all neccessary code

--- webapp/app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-# Key will only need to be exposed once and can (and has to!) be deleted after that.
-# openai.api_key=''
-
-app = Flask(__name__)
-
-from app import routes
