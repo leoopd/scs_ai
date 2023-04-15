d257a0b87e0ee9e4b678fbd07951df14fcfebee6
Leo <109588159+leoopd@users.noreply.github.com>
2023-04-15 20:13:32 +0200

    changed webapp.py to contain all flask code
    splitting it into multiple files does not seem to be neccessary for this small app

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -1,1 +1,34 @@
-from app import app
+from flask import Flask, request, render_template
+import openai
+import os
+
+# Key will only need to be exposed once and can (and has to!) be deleted after that.
+# openai.api_key=''
+
+app = Flask(__name__)
+
+# Will display the configured webpage
+@app.route('/scsai')
+def page():
+    return render_template('/home/code-server/code-server-app/python/webapp/flaskapp/ai_template.html')
+
+# Will process the webapp with the model and settings specified.
+@app.route('/scsai_processing', methods=['POST'])
+def scs_ai():
+    user_input = request.form['user_input']
+
+    response = openai.Completion.create(
+        engine='scs-faq-domains-2023-04-13-09-28-43',
+        prompt=user_input,
+        temperature=0,
+        max_tokens=1000,
+        n=1,
+        stop=[" ###"]
+    )
+
+    ai_output = response.choices[0].text.strip()
+
+    return render_template('/home/code-server/code-server-app/python/webapp/flaskapp/ai_template.html', output=ai_output)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
