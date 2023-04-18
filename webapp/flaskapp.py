7a48abf02b1b7abd664ba721b6e0d4e6c52f47ec
Leo <109588159+leoopd@users.noreply.github.com>
2023-04-18 21:42:46 +0200

    prepared the app to write data to a db
    will write user_input, the corresponding response and (if given) a rating for the response to a db

--- webapp/flaskapp.py
+++ webapp/flaskapp.py
@@ -11,23 +11,30 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def scs_ai():
     if request.method == 'POST':
-        user_input = 'Domains, ' + request.form['user_input'] + '\'\\n\\n###\\n\\n'
-        # output = f'You entered {user_input}'
 
-        response = openai.Completion.create(
-            engine='curie:ft-personal:scs-faq-domains-2023-04-13-09-28-43',
-            prompt=user_input,
-            max_tokens=300,
-            n=1,
-            stop=[" ###"],
-            frequency_penalty=1
-        )
+        # If user fills the prompt-textbox create a response, return it and write user_input
+        # and response to the database.
+        if user_input:
+            user_input = 'Domains, ' + request.form['user_input'] + '\'\\n\\n###\\n\\n'
 
-        output = response.choices[0].text.strip()
+            response = openai.Completion.create(
+                engine='curie:ft-personal:scs-faq-domains-2023-04-13-09-28-43',
+                prompt=user_input,
+                max_tokens=300,
+                n=1,
+                stop=[" ###"],
+                frequency_penalty=1
+            )
+            
+            output = response.choices[0].text.strip()
 
-        return render_template('ai_template.html', output=output, user_input=user_input)
-    else:
-        return render_template('ai_template.html')
+            return render_template('ai_template.html', output=output, user_input=user_input)
+        else:
+            return render_template('ai_template.html')
+
+        # If user fills the rating-textbox write the rating to the database.
+        if user_rating:
+            
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
